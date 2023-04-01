fe779fc5c178f0d72f3585088205c2d18e7fa27a
Gabriel Terwesten <gabriel@terwesten.net>
2023-04-01 11:30:31 +0200

    verbose logging

--- packages/runner_flutter/integration_test/benchmark_test.dart
+++ packages/runner_flutter/integration_test/benchmark_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:runner_flutter/main.dart' as runner_flutter;
@@ -8,9 +7,8 @@ Future<void> main() async {
 
   testWidgets('run benchmarks', (_) async {
     final benchmarkResults = await runner_flutter.main();
-    final reportData =
-        (WidgetsBinding.instance as IntegrationTestWidgetsFlutterBinding)
-            .reportData ??= <String, dynamic>{};
+    final reportData = IntegrationTestWidgetsFlutterBinding
+        .instance.reportData ??= <String, Object?>{};
     reportData.addAll(benchmarkResults);
   });
 }
